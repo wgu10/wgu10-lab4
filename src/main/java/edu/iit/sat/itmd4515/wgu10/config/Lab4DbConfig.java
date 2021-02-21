29cfcfde1aba62d12832ded59e66e62cdb8f7f15
ext.yuchangcheng3 <ext.yuchangcheng3@jd.com>
2021-02-22 00:03:58 +0800

    guwengan

---
+++ src/main/java/edu/iit/sat/itmd4515/wgu10/config/Lab4DbConfig.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.iit.sat.itmd4515.wgu10.config;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+   com.mysql.jdbc.Driver
+ * com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+ * @author wgu10
+ */
+@ApplicationScoped
+@DataSourceDefinition(
+        name = "java:app/jdbc/itmd4515DS",
+        className = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource",
+        serverName = "localhost",
+        portNumber = 3306,
+        databaseName = "Chinook",
+        user = "itmd4515",
+        password = "itmd4515",
+        properties = {
+            "characterEncoding=utf8",
+            "useSSL=false",
+            "serverTimezone=UTC"            
+        }
+)
+public class Lab4DbConfig {
+    
+}
---
+++ src/main/java/edu/iit/sat/itmd4515/wgu10/web/DemoServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.iit.sat.itmd4515.wgu10.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author wgu10
+ */
+@WebServlet(name = "DemoServlet", urlPatterns = {"/demo"})
+public class DemoServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        
+        response.sendRedirect("http://www.iit.edu");
+        
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DemoServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DemoServlet doGet " + request.getContextPath() + "</h1>");
+            out.println("Now is: " + LocalDateTime.now());
+            out.println("</html>");
+        }
+        
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String fn = request.getParameter("custFirstName");
+        String ln = request.getParameter("custLastName");
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DemoServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DemoServlet doPost " + request.getContextPath() + "</h1>");
+            out.println("<h2>" + fn + " " + ln + "</h2>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
